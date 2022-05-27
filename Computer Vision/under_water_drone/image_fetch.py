6fc4f359a4c57af9117ac32f38e98e1f3806b6ef
AllanMisasa <68166799+AllanMisasa@users.noreply.github.com>
2022-05-27 07:56:40 +0200

    Edited code for the working dataset

--- Computer Vision/under_water_drone/image_fetch.py
+++ Computer Vision/under_water_drone/image_fetch.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 # url2 = 'https://raw.githubusercontent.com/AllanMisasa/Python-ML-pipeline-modules/main/Computer%20Vision/DD_dataset/'
-url = "https://github.com/AllanMisasa/Python-ML-pipeline-modules/blob/main/Computer%20Vision/DD_dataset" # url for the images
+# url = "https://github.com/AllanMisasa/Python-ML-pipeline-modules/blob/main/Computer%20Vision/DD_dataset" # url for the images
 
 def get_url_paths(url, ext='', params={}): # Gets the paths of the images from the url
     response = requests.get(url, params=params) # Gets the html from the url
@@ -15,11 +14,11 @@ def get_url_paths(url, ext='', params={}): # Gets the paths of the images from t
     parent = [url + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext)] # Gets the links from the html
     return parent # Returns the links
 
-ext = '.jpg' # The extension of the images
+# ext = '.jpg' # The extension of the images
 
 def url_strip(url): # Strips the url of the image
     start = 18 
-    stop = 96
+    stop = 108
     first = url[:start] # Gets the first part of the url
     second = url[stop:] # Gets the second part of the url
     combined = first + second # Combines the two parts
@@ -27,10 +26,10 @@ def url_strip(url): # Strips the url of the image
     combined = combined.replace('https://github.com/', 'https://raw.githubusercontent.com/') # Replaces the github url with the raw github url
     return combined # Returns the combined url
 
-image_paths = [url_strip(url) for url in get_url_paths(url, ext)] # Gets the paths of the images from the url
+# image_paths = [url_strip(url) for url in get_url_paths(url, ext)] # Gets the paths of the images from the url
 
 #image_paths = get_url_paths(url2, ext)
-print((image_paths[0])) # Prints the first image path
+# print((image_paths[0])) # Prints the first image path
 
 # example_image = Image.open(urllib.request.urlopen(image_paths[3])) # Opens the first image
 # gray = ImageOps.grayscale(example_image) # Converts the image to grayscale
--- Computer Vision/under_water_drone/image_preprocessing.py
+++ Computer Vision/under_water_drone/image_preprocessing.py
@@ -4,9 +4,12 @@ import numpy as np
 from image_fetch import get_url_paths, url_strip 
 
 
-url = "https://github.com/AllanMisasa/Python-ML-pipeline-modules/blob/main/Computer%20Vision/DD_dataset"
+# url = "https://github.com/AllanMisasa/Python-ML-pipeline-modules/blob/main/Computer%20Vision/DD_dataset"
+url = "https://github.com/AllanMisasa/Python-ML-pipeline-modules/blob/main/Computer%20Vision/DD_dataset/transformed"
+
 ext = '.jpg'
 image_paths = [url_strip(url) for url in get_url_paths(url, ext)] # Gets the paths of the images from the url
+print(image_paths[3])
 
 def get_image(number): # Gets the image from the url
     req = request.urlopen(image_paths[number]) # Opens the url
@@ -19,15 +22,15 @@ image = get_image(3) # Gets the image
 def preprocess_image(image): # Preprocesses the image
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY) # Converts the image to grayscale
     img_blur = cv.GaussianBlur(gray, (3, 3), 0) # Blurs the image
-    dimensions = np.shape(gray) # The shape of the image
-    cropped_image = gray[80:dimensions[1], 150:330] # Crops the image
+    #dimensions = np.shape(gray) # The shape of the image
+    # cropped_image = gray[80:dimensions[1], 150:330] # Crops the image
     # img_thresh = cv.adaptiveThreshold(img_blur, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 11, 2) # Thresholds the image
     sobelx = cv.Sobel(img_blur, cv.CV_64F, 1, 0, ksize=5) # Applies the sobel filter to the x axis of the image
     sobely = cv.Sobel(img_blur, cv.CV_64F, 0, 1, ksize=5)  # Applies the sobel filter to the y axis of the image
     sobelxy = cv.Sobel(img_blur, cv.CV_64F, 1, 1, ksize=5) # Applies the sobel filter to the x and y axis of the image
     edges = cv.Canny(img_blur, 100, 200) # Applies the canny filter to the image
     ret, thresh = cv.threshold(gray, 127, 255, cv.THRESH_BINARY) # Thresholds the image
-    return cropped_image
+    return gray
 
 processed_image = preprocess_image(image)
     
